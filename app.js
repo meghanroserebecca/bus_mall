869217bf65e54de34846a32299bf8d3fdf46cbd7
meghanroserebecca <meghanroserebecca@gmail.com>
2016-11-28 15:11:47 -0800

    altered HTML to show 3 images, an h1, an h2 and a paragraph; altered app.js to randomize all 3 images when one is clicked with no repeats -- i think; altered style.css to get images to line up side by side

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var paths = ['sweep.png', 'dog-duck.jpg', 'breakfast.jpg'];
+var paths = ['sweep.png', 'dog-duck.jpg', 'breakfast.jpg', 'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'tauntaun.jpg', 'unicorn.jpg','usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 var displayIndex = 0;
 var items = [];
 
 var displayArea = document.getElementById('image_area');
 
-for(var i=0; i < paths.length; i++){
+for(var i = 0; i < paths.length; i++){
   var newItem = new ItemImage(paths[i]); //creates a new ItemImage instance and attaches it to our object
   items.push(newItem); //pushes to an array of all of our items
 }
@@ -15,11 +15,11 @@ displayArea.addEventListener('click', clickHandler);
 
 function clickHandler(event) {
   var targetString = event.target.src;
-  var targetPath = targetString.split('assets')[1];
+  var targetPath = targetString.split('bus_mall')[1];
   var itemPath;
 
   for (var i = 0; i < items.length; i++) {
-    itemPath = items[i].path.split('assets')[1];
+    itemPath = items[i].path.split('bus_mall')[1];
     if (itemPath === targetPath) {
       items[i].clicked += 1;
     }
@@ -34,6 +34,8 @@ function ItemImage (path) {
 
 function changePicture() {
   var imageOne = document.getElementById('image_one');
+  var imageTwo = document.getElementById('image_two');
+  var imageThree = document.getElementById('image_three');
   var randomIndex = generateRandomNumber();
 
   while (displayIndex === randomIndex) {
@@ -41,7 +43,17 @@ function changePicture() {
   }
 
   displayIndex = randomIndex;
-  imageOne.src = '../bus_mall/assets/' + paths[randomIndex];
+  imageOne.src = '../bus_mall/assets/' + paths[randomIndex]; //need to get these to turn into different images from each other - loop?
+  imageTwo.src = '../bus_mall/assets/' + paths[generateRandomNumber()];
+  imageThree.src = '../bus_mall/assets/' + paths[generateRandomNumber()];
+
+  while (imageTwo.src === imageOne.src) {
+    imageTwo.src = '../bus_mall/assets/' + paths[generateRandomNumber()];
+  }
+
+  while (imageThree.src === imageOne.src || imageThree.src === imageTwo.src) {
+    imageThree.src = '../bus_mall/assets/' + paths[generateRandomNumber()];
+  }
 
   function generateRandomNumber() {
     return Math.floor(Math.random() * paths.length);
